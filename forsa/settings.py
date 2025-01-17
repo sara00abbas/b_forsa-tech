a838ed913103479b7bdbc6961c9861b5c12c0807
Sedraalsabagh <sedraalsabbagh2021@gmail.com>
2025-01-17 21:42:57 +0300

    connect with postgraSQL

--- forsa/settings.py
+++ forsa/settings.py
@@ -75,12 +75,14 @@ WSGI_APPLICATION = 'forsa.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'forsa',
+        'USER':'postgres',
+        'PASSWORD':'1234',
+        'HOST':'localhost',
+        'POST':'5432'
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
