6851f0476938fa32923d590225961bc97fdea7da
sara00abbas <sara.abbas.ab@gmail.com>
2025-02-04 23:38:53 +0300

    register & login

--- Devloper/views.py
+++ Devloper/views.py
@@ -6,24 +6,23 @@ from django.contrib.auth.hashers import make_password
 from rest_framework import status
 from .serializer import SingUpSerializer
 
-User = get_user_model()  # استخدام نموذج المستخدم المخصص
+User = get_user_model()  
 
 @api_view(['POST'])
 def login(request):
     data = request.data
     
-    # التحقق من صحة البيانات باستخدام Serializer
+   
     serializer = SingUpSerializer(data=data)
     if serializer.is_valid():
         email = data.get('email')
         username = data.get('username')
         password = data.get('password')
 
-        # التحقق مما إذا كان المستخدم موجودًا بالفعل
         if User.objects.filter(email=email).exists():
             return Response({'error': 'This email already exists!'}, status=status.HTTP_400_BAD_REQUEST)
 
-        # إنشاء المستخدم الجديد
+       
         user = User.objects.create(
             email=email,
             username=username,
