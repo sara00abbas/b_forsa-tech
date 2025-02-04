fce206c55cc0ed5b26bfe1b11df0d673597263c6
sara00abbas <sara.abbas.ab@gmail.com>
2025-02-04 23:36:07 +0300

    register & log in

--- Devloper/serializer.py
+++ Devloper/serializer.py
@@ -1,18 +1,14 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model  
 
+User = get_user_model() 
 
 class SingUpSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('first_name','last_name', 'email', 'password')
+        fields = ['email', 'username', 'password']
+        extra_kwargs = {'password': {'write_only': True}}  
 
-        extra_kwargs = {
-            'first_name': {'required':True ,'allow_blank':False},
-            'last_name' : {'required':True ,'allow_blank':False},
-            'email' : {'required':True ,'allow_blank':False},
-            'password' : {'required':True ,'allow_blank':False,'min_length':8}
-        }
 
 
 # class LoginSerializer(serializers.ModelSerializer):
--- forsa/asgi.py
+++ forsa/asgi.py
@@ -13,4 +13,4 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'forsa.settings')
 
-application = get_asgi_application() #sedra #sedra
+application = get_asgi_application()
