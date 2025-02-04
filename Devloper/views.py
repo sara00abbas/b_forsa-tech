93ca1fc445c32d4d9f9d07f129e04d0deb8ec858
sara00abbas <sara.abbas.ab@gmail.com>
2025-02-05 02:55:06 +0300

    register edit

--- Devloper/views.py
+++ Devloper/views.py
@@ -9,7 +9,7 @@ from .serializer import SingUpSerializer
 User = get_user_model()  # استخدام نموذج المستخدم المخصص
 
 @api_view(['POST'])
-def login(request):
+def register(request):
     data = request.data
     
     # التحقق من صحة البيانات باستخدام Serializer
