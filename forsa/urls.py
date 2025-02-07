8b8e24328e8c698022c986794452f7fcd5241240
safaabouzaid <abouzaidsafa@gmail.com>
2025-02-07 19:32:35 +0300

    Add admin login API and update authentication setup

--- forsa/urls.py
+++ forsa/urls.py
@@ -3,9 +3,17 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import path, include
 
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include("devloper.urls"))
+    path('',include("devloper.urls")),
+    path('api/admin/login/', TokenObtainPairView.as_view(), name='admin_login'),
+
+
     
 ]
+
+
