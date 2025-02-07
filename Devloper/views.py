0cd94d13c3c5f3a921b405fbc7575202b224c5d1
sara00abbas <sara.abbas.ab@gmail.com>
2025-02-07 18:57:48 +0300

    edit

--- Devloper/views.py
+++ Devloper/views.py
@@ -6,28 +6,19 @@ from django.contrib.auth.hashers import make_password
 from rest_framework import status
 from .serializer import SingUpSerializer
 
-<<<<<<< HEAD
 User = get_user_model()  
-=======
-User = get_user_model()  # استخدام نموذج المستخدم المخصص
->>>>>>> 93ca1fc445c32d4d9f9d07f129e04d0deb8ec858
 
 @api_view(['POST'])
 def login(request):
     data = request.data
     
-<<<<<<< HEAD
     
-=======
-    # التحقق من صحة البيانات باستخدام Serializer
->>>>>>> 93ca1fc445c32d4d9f9d07f129e04d0deb8ec858
     serializer = SingUpSerializer(data=data)
     if serializer.is_valid():
         email = data.get('email')
         username = data.get('username')
         password = data.get('password')
 
-<<<<<<< HEAD
         
         if User.objects.filter(email=email).exists():
             return Response({'error': 'This email already exists!'}, status=status.HTTP_400_BAD_REQUEST)
@@ -37,17 +28,6 @@ def login(request):
             email=email,
             username=username,
             password=make_password(password),  
-=======
-        # التحقق مما إذا كان المستخدم موجودًا بالفعل
-        if User.objects.filter(email=email).exists():
-            return Response({'error': 'This email already exists!'}, status=status.HTTP_400_BAD_REQUEST)
-        
-        # إنشاء المستخدم الجديد
-        user = User.objects.create(
-            email=email,
-            username=username,
-            password=make_password(password),  # تأكد من تشفير كلمة المرور
->>>>>>> 93ca1fc445c32d4d9f9d07f129e04d0deb8ec858
         )
 
         return Response({'details': 'Your account registered successfully'}, status=status.HTTP_201_CREATED)
--- forsa/urls.py
+++ forsa/urls.py
@@ -6,10 +6,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-<<<<<<< HEAD
     path('auth/',include("devloper.urls"))
-=======
-    path('',include("devloper.urls"))
->>>>>>> 93ca1fc445c32d4d9f9d07f129e04d0deb8ec858
     
 ]
